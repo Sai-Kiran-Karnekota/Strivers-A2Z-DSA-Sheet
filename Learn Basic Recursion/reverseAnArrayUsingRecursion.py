ed643072360bf17956cd82d9f3c54ff5c46199e9
Karnekota Sai Kiran <saikirankarnekota@gmail.com>
2023-05-17 19:14:10 +0530

    Update reverseAnArrayUsingRecursion.py

--- Learn Basic Recursion/reverseAnArrayUsingRecursion.py
+++ Learn Basic Recursion/reverseAnArrayUsingRecursion.py
@@ -9,3 +9,19 @@ def rev(c):
         return rev(c)
     return L
 print(rev(a-1))
+
+
+# Another approach
+
+Arr = [1,2,3,4,5]
+a = len(Arr)
+b = a//2 if a%2==0 else a//2 + 1
+def rev(c):
+    if c >= b:
+        d = Arr[c]
+        Arr[c] = Arr[a-c-1]
+        Arr[a-c-1] = d
+        c -= 1
+        return rev(c)
+    return Arr
+print(rev(a-1))
