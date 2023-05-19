7a504078f6400645f45b423d532baf72ed1933c9
Karnekota Sai Kiran <saikirankarnekota@gmail.com>
2023-05-19 22:33:22 +0530

    Update Largest element in an Array.py

--- Arrays/Easy/Largest element in an Array.py
+++ Arrays/Easy/Largest element in an Array.py
@@ -1,7 +1,17 @@
 def largest( arr, n):
-    max=arr[0]
+    m = arr[0]
+    for i in arr:
+        if m < i:
+            m = i
+            
+    return m
+
+# Another approach
+
+def largest( arr, n):
+    m = float("-inf")
     for i in arr:
         if max < i:
-            max = i
+            m = i
             
-    return max
+    return m
