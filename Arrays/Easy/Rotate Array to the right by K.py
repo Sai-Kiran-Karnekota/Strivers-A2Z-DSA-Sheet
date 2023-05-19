09f4a381f415adba7b657ca102d36d547c73e29c
Karnekota Sai Kiran <saikirankarnekota@gmail.com>
2023-05-19 17:03:22 +0530

    Create Rotate Array to the right by K.py

---
+++ Arrays/Easy/Rotate Array to the right by K.py
@@ -0,0 +1,8 @@
+
+def rotate(self, nums: List[int], k: int) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    if k > len(nums):
+        k = k%len(nums)
+    nums[:] = nums[-k:] + nums[:-k]
